31b78d495f4b1a489a5dc039cbc45e7e38695741
Damon On <onddamon@gmail.com>
2018-06-19 02:13:32 -0400

    Server and graphQL Setup

---
+++ slack-clone-server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models from './models';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+// const myGraphQLSchema = // ... define or import your schema here!
+const PORT = 8080;
+
+const app = express();
+
+const graphqlEndpoint = '/graphql';
+// bodyParser is needed just for POST.
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
+
+models.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT);
+});
+
