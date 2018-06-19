938de3a2a2e5b086ba2ad66165d71a75014514b3
Damon On <onddamon@gmail.com>
2018-06-19 12:31:45 -0400

    removed unnecessary underscores

--- slack-clone-server/models/index.js
+++ slack-clone-server/models/index.js
@@ -1,6 +1,6 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('slack', 'postgres', 'postgres', { dialect: 'postgres' });
+const sequelize = new Sequelize('slack', 'postgres', 'postgres', { dialect: 'postgres', define: { underscored: true } });
 
 const models = {
   User: sequelize.import('./user'),
