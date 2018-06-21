aed9e564da10b80c5607a40707d27daa52ae82c0
Damon On <onddamon@gmail.com>
2018-06-21 17:53:43 -0400

    Add Middleware and Afterware Apollo Client

---
+++ slack-clone-client/slack-clone-client/src/routes/CreateTeam.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Container, Header, Input, Button, Form, Message } from 'semantic-ui-react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+class CreateTeam extends React.Component {
+  constructor(props) {
+    super(props);
+
+    extendObservable(this, {
+      name: '',
+      errors: {},
+    });
+  }
+
+  onSubmit = async () => {
+    const { name } = this;
+    const response = await this.props.mutate({
+      variables: { name },
+    });
+    console.log(response);
+    const {
+      ok, errors,
+    } = response.data.createTeam;
+
+    if (ok) {
+      this.props.history.push('/');
+    } else {
+      const err = {};
+      errors.forEach(({ path, message }) => {
+        err[`${path}Error`] = message;
+      });
+      this.errors = err;
+    }
+  }
+
+  onChange = (e) => {
+    const { name, value } = e.target;
+    this[name] = value;
+  }
+
+  render() {
+    const { name, errors: { nameError } } = this;
+
+    const errorList = [];
+
+    if (nameError) {
+      errorList.push(nameError);
+    }
+
+    return (
+      <Container text>
+        <Header as="h2">Create Team</Header>
+        <Form>
+          <Form.Field error={!!nameError}>
+            <Input
+              name="name"
+              onChange={this.onChange}
+              value={name}
+              placeholder="name"
+              fluid
+            />
+          </Form.Field>
+          <Button onClick={this.onSubmit}>Submit</Button>
+        </Form>
+        {errorList.length ? (
+          <Message
+            error
+            header="There was some errors with your submission"
+            list={errorList}
+          />
+        ) : null}
+      </Container>
+    );
+  }
+}
+
+const createTeamMutation = gql`
+  mutation($name: String!) {
+    createTeam(name: $name) {
+      ok
+      errors{
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(createTeamMutation)(observer(CreateTeam));
--- slack-clone-server/schema/user.js
+++ slack-clone-server/schema/user.js
@@ -23,6 +23,7 @@ export default `
     refreshToken: String
     errors: [Error!]
   }
+  
   type Mutation {
     register(username: String!, email: String!, password: String!): RegisterResponse!
     login(email: String!, password: String!): LoginResponse!
