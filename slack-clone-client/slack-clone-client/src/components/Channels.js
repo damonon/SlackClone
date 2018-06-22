074218242b30372701c41dc86f8650c4a0c200ad
Damon On <onddamon@gmail.com>
2018-06-22 02:43:33 -0400

    Update Sidebar Styled Component

--- slack-clone-client/slack-clone-client/src/components/Channels.js
+++ slack-clone-client/slack-clone-client/src/components/Channels.js
@@ -5,34 +5,72 @@ const ChannelWrapper = styled.div`
   grid-column: 2;
   grid-row: 1 / 4;
   background-color: #4e3a4c;
-  color: white;
+  color: #958993;
 `;
 
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
+
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
+
+const paddingLeft = 'padding-left: 10px;';
+
+const SideBarListItem = styled.li`
+  padding: 2px;
+  ${paddingLeft};
+
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const SideBarListHeader = styled.li`
+  ${paddingLeft};
+`;
+
+const PushLeft = styled.div`
+  ${paddingLeft};
+`;
+
+const Green = styled.span`
+  color: #38978d;
+`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : '○');
+
 const channel = ({ id, name }) => (
-  <li key={`channel-${id}`}># {name}</li>
+  <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>
 );
 
 const user = ({ id, name }) => (
-  <li key={`user-${id}`}>{name}</li>
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble />{name}
+  </SideBarListItem>
 );
 
 export default ({
   teamName, username, channels, users,
 }) => (
   <ChannelWrapper>
-    <div>
-      {teamName}
+    <PushLeft>
+      <TeamNameHeader>{teamName}</TeamNameHeader>
       {username}
-    </div>
+    </PushLeft>
     <div>
-      <ul>
-        <li>Channels</li>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
         {channels.map(channel)}
-      </ul>
-      <ul>
-        <li>Direct Messages</li>
+      </SideBarList>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
         {users.map(user)}
-      </ul>
+      </SideBarList>
     </div>
   </ChannelWrapper>
 );
